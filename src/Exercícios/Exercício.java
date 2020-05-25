a2f5abae7cf7caecb740ac91985de53f3062e73b
unknown <labreu690@gmail.com>
2020-05-25 18:49:03 -0300

    Exercício 4

--- src/Exercícios/Exercício.java
+++ src/Exercícios/Exercício.java
@@ -7,45 +7,22 @@ public class Exercício {
 	public static void main(String[] args) {
 		
 		Scanner leitor = new Scanner (System.in);
-		double x, salario = 0;
-		double alturaA = 0 , alturaB = 0; 
-		int sexo; 
-		int mulher = 0;
+		int x, contagem = 1;
+		int maior = 0;	
 		
-		
-		for(int i = 1; i <= 20; i++) {
+		for(int i = 1; i <= 15; i++) {
 			
-			System.out.println("Digite a  Altura do "+i+"º Funcionário: ");
-			x = leitor.nextFloat();
-			
-			if(x > alturaA) {
-				alturaA = x;
+			System.out.println("Digite o "+i+"º Número: ");
+		}
+		do {
+			System.out.printf("%d° número: ", contagem);
+			x = leitor.nextInt();
+			contagem++;
+			if (x > maior) {
+				maior = x;
 			}
-			else if(x < alturaB) {
-				alturaB = x;
-			}	
-			
-			do {
-				
-				System.out.println("Digite o Sexo do Funcionário: Digite 1 para Feminino e 2 para Masculino ");
-				sexo = leitor.nextInt();
-								
-			} while(!(sexo == 1 || sexo == 2));
-			
-			
-			System.out.println("Digite o Salário do Funcionário: ");
-			
-			double dinheiro = leitor.nextFloat();
-			
-			if(dinheiro > 1000 && sexo == 2) {
-				mulher++;
-			}
-			salario += dinheiro;
-	    }
-		System.out.println("A Média de Salário Entre Os Funcionários É: "+salario / 20);
-		System.out.println("A Maior Altura Entre Os Funcionários É: " +alturaA);
-		System.out.println("A Menor Altura Entre Os Funcionários É: " +alturaB);
-		System.out.println("A Quantidade de Mulheres na Empresa Que Recebem R$1000,00 É: "+mulher);
-    }
+		}
+		while (contagem <= 15);
+		System.out.printf("O Número é: "+maior);
+	}
 }
-
