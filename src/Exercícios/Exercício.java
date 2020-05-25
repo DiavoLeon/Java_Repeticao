5573587a8789c35f235bd7e65d9947b983b72fee
unknown <labreu690@gmail.com>
2020-05-25 15:27:25 -0300

    Exercício 2

--- src/Exercícios/Exercício.java
+++ src/Exercícios/Exercício.java
@@ -7,7 +7,16 @@ public class Exercício {
 	public static void main(String[] args) {
 		
 		Scanner leitor = new Scanner (System.in);
-
+		
+		int x = 0;
+		
+		System.out.println("Digite um Número: ");
+		x = leitor.nextInt();
+		
+		for (int i = 1; i <= x; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("FIM DO LOOP");
 	}
-
 }
