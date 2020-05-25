93b544308f02898328dfd01d67c2a1cda47c26e9
unknown <labreu690@gmail.com>
2020-05-25 15:17:20 -0300

    Exercício 1

--- src/Exercícios/Exercício.java
+++ src/Exercícios/Exercício.java
@@ -7,7 +7,25 @@ public class Exercício {
 	public static void main(String[] args) {
 		
 		Scanner leitor = new Scanner (System.in);
-
+		
+		for (int i = 1; i < 11; i++) {
+			
+			int x = 0;
+			
+			System.out.println("Digite o "+i+"º Número: ");
+			x = leitor.nextInt();
+			
+			if (x > 0) {
+				System.out.println("POSITIVO");
+					
+			}
+			else if (x < 0) {
+				System.out.println("NEGATIVO");
+			}
+			else {
+				System.out.println("NEUTRO");
+			}
+		}
+		System.out.println("FIM DO LOOP");
 	}
-
 }
