ffa6bddf01b4e45f62ef013ceb904e9d187a0eb5
unknown <labreu690@gmail.com>
2020-05-25 14:24:37 -0300

    Base do Projeto

---
+++ src/Exercícios/Exercício.java
@@ -0,0 +1,13 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class Exercício {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner (System.in);
+
+	}
+
+}
