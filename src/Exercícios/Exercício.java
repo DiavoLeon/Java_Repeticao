0289ba7ab71a54e7d936f0b9463600a890b50611
unknown <labreu690@gmail.com>
2020-05-25 15:37:45 -0300

    Exercício 3

--- src/Exercícios/Exercício.java
+++ src/Exercícios/Exercício.java
@@ -7,7 +7,16 @@ public class Exercício {
 	public static void main(String[] args) {
 		
 		Scanner leitor = new Scanner (System.in);
-
+		
+		long x = 0;
+						
+		for (int i = 1; i <= 20; i++) {
+			
+			System.out.println("Digite o "+i+"º Número: ");
+			x += leitor.nextInt();
 	}
-
+		
+		System.out.println("A Soma dos Valores é: "+ x);
+		System.out.println("A Média dos Valores é: "+ x / 20);
+	}
 }
