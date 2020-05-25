4c72a9352a1f6b0010bbab74a8b2a75407e50148
unknown <labreu690@gmail.com>
2020-05-25 15:53:47 -0300

    Exercício 4

--- src/Exercícios/Exercício.java
+++ src/Exercícios/Exercício.java
@@ -7,7 +7,17 @@ public class Exercício {
 	public static void main(String[] args) {
 		
 		Scanner leitor = new Scanner (System.in);
+		int x = 0;
+		
+		for(int i = 1; i <= 15; i++) {
+			System.out.println("Digite o "+i+"º Número");
+			int y = leitor.nextInt();
+			
+			x = (y > x) ? y : x;
+        }
+        
+        System.out.print("O maior valor digitado foi "+x);			
+		}
 
 	}
 
-}
